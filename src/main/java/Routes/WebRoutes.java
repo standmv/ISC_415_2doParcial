37989893b2ccb8c1eb9c2481d6f55e129cad8b5a
Stanley De Moya <standmv13@gmail.com>
2018-07-22 17:54:04 -0400

    implementando registrar(incompleto)

--- src/main/java/Routes/WebRoutes.java
+++ src/main/java/Routes/WebRoutes.java
@@ -6,6 +6,8 @@ import spark.QueryParamsMap;
 import Dao.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import encapsulation.*;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,8 +15,10 @@ import static spark.Spark.*;
 public class WebRoutes {
     public WebRoutes(final FreeMarkerEngine freeMarkerEngine){
         UserDaoImpl usuarioDao;
+        ProfileDaoImpl profileDao;
 
         usuarioDao = new UserDaoImpl(User.class);
+        profileDao = new ProfileDaoImpl(Profile.class);
 
         get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
@@ -39,6 +43,50 @@ public class WebRoutes {
             return new ModelAndView(attributes, "registrar.ftl");//registrar
         }, freeMarkerEngine);
 
+        post("/registrarse", (request, response) -> {
+
+            QueryParamsMap map = request.queryMap();
+            SimpleDateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+            User usuario = new User();
+            Profile profile = new Profile();
+            usuario.setUsername(map.get("username").value());
+            usuario.setEmail(map.get("email").value());
+            usuario.setPassword(map.get("password").value());
+            usuarioDao.getProfile(usuario).setNombre(map.get("nombre").value());
+            usuarioDao.getProfile(usuario).setApellido(map.get("apellido").value());
+            usuarioDao.getProfile(usuario).setDireccion(map.get("ciudad").value());
+            java.util.Date fechanacimiento = format.parse(request.queryParams("fechanacimiento"));
+            usuarioDao.getProfile(usuario).setLugarestudio(map.get("lugarestudio").value());
+            usuarioDao.getProfile(usuario).setTrabajo(map.get("lugartrabajo").value());
+            //  usuarioDao.getProfile(usuario.getId()).setSexo(map.get("sexo").value());
+            if(request.queryParams("rol")!=null){
+                if(request.queryParams("rol").equals( "administrator")){
+
+                    usuario.setPriviledge(true);
+                }
+
+                else{
+                    usuario.setPriviledge(false);
+                }}
+
+            UserDaoImpl userDao = null;
+
+            if(userDao.searchByUsername(usuario.getUsername())==null){
+
+                userDao.add(usuario);
+                profileDao.add(userDao.getProfile(usuario));
+                response.redirect("/");
+
+                return null;
+            }
+            else {
+                return "Usuario ya existe!";
+            }
+
+        });
+
+
+        /*
         get("/home", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             return new ModelAndView(attributes, "home.ftl");//home
@@ -54,7 +102,7 @@ public class WebRoutes {
             return new ModelAndView(attributes, "fotos.ftl");//fotos
         }, freeMarkerEngine);
 
-
+        */
 
 
     }
