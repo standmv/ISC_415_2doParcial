93e877de0e467f73e948f5687d71b8f9b1845620
Stanley De Moya <standmv13@gmail.com>
2018-07-21 21:00:18 -0400

    Lo que se tiene hasta ahora

--- src/main/java/encapsulation/User.java
+++ src/main/java/encapsulation/User.java
@@ -1,4 +1,39 @@
 package encapsulation;
 
-public class User {
+
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="users")
+@Where(clause = "deleted = 0")
+
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    @Column(name="id", nullable = false, unique = true, updatable = false)
+    private int id;
+
+    @NotEmpty
+    @Column(name = "username")
+    private String username;
+
+    @NotEmpty(message="La contrasena no puede estar vacia")
+    @Column(name = "password")
+    private String password;
+
+    @NotEmpty(message="El correo no puede estar vacio")
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "priviledge")
+    private boolean priviledge;
+
+    @Column(name="deleted")
+    private boolean deleted = false;
+
 }
