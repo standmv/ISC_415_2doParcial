80f004f048f6751a1258df857a6bcf451aa2b822
Stanley De Moya <standmv13@gmail.com>
2018-08-29 04:09:12 -0400

    Crear post REST

--- src/main/java/ClienteRest.java
+++ src/main/java/ClienteRest.java
@@ -2,6 +2,8 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
+import javax.ws.rs.core.MultivaluedMap;
+
 
 public class ClienteRest {
     public ClienteRest ()
@@ -19,5 +21,13 @@ public class ClienteRest {
         return salida;
     }
 
+    public String crearPost (MultivaluedMap formData)
+    {
+        Client cliente = Client.create();
+        WebResource recursos = cliente.resource("http://localhost:4567/rest/");
+        ClientResponse respuesta = recursos.type("application/x-www-form-urlencoded").post(ClientResponse.class, formData);
+        String salida = respuesta.getEntity(String.class);
+        return salida;
+    }
 
 }
